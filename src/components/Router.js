7ed4901a90fe8104ec825f799f7a572d5c16d6e5
hands8142 <hands8142@naver.com>
2020-09-27 14:32:09 +0900

    1.0.0

---
+++ src/components/Router.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
+
+const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation userObj={userObj} />}
+      <Switch>
+        {isLoggedIn ? (
+          <React.Fragment>
+            <div
+              style={{
+                maxWidth: 890,
+                width: "100%",
+                margin: "0 auto",
+                marginTop: 80,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Route exact path="/">
+                <Home userObj={userObj} />
+              </Route>
+              <Route exact path="/profile">
+                <Profile userObj={userObj} refreshUser={refreshUser} />
+              </Route>
+            </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </React.Fragment>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
